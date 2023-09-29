715a51538783a63ef0a0636eb4c37500589709b2
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-09-29 21:47:49 +0530

    Area Of Rectangle Program

---
+++ Basic/Area of Rectangle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	double l,b ;
+	cout << "Write the length of the rectangle: ";
+	cin >> l;
+	cout << "Write the breadth of the rectangle: ";
+	cin >> b;
+	cout << "Area of the rectangle is: " << l*b << endl ;
+}
