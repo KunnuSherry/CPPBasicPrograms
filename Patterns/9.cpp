cae191145ebee3b0df197fcf0f65e8f48b9a5616
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-09-30 10:30:09 +0530

    Create 9.cpp

---
+++ Patterns/9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std ;
+int main(){
+	int n = 4 ;	
+	int i = 1 ;
+	int k = 1 ;
+	while(i<=n){
+		int j = 1 ;
+		k = i ;
+		while (j<=i){
+			cout << k << " " ;
+			j = j+1 ;
+			k = k-1 ;
+		}
+		cout << endl ;
+		i = i+1 ;
+	}
+}
+
+// 1
+// 2 3
+// 3 4 5
+// 4 5 6 7
