ec7882ebe63e6ca452cdb6a21af8dba33587a580
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-10-08 12:34:38 +0530

    Create 16.cpp

---
+++ Patterns/16.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std ;
+int main(){
+	int a = 5 ;
+	int i ;
+	for(i=1; i<=a; i++){
+		for(int j =1; j<=i-1; j++){
+			cout << " ";
+		}
+		for(int j =1; j<=5-i+1; j++){
+			cout <<"*";
+		}
+		cout << endl ; 
+	}
+}
+
+
+// *****
+//  ****
+//   ***
+//    **
+//     *
