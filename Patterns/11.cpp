afd1563bdfc06718e44d7be89c4592c23b383255
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-10-08 12:12:08 +0530

    Create 11.cpp

---
+++ Patterns/11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+int main(){
+	int a = 5 ;
+	int i ;
+	for(i=1; i<=a; i++){
+		for(int j = 1; j<=i; j++){
+			cout << j <<" ";
+		}
+		cout << endl ;
+	}
+}
+
