a082f01f5c87d0b2ba9b398bf231ebc628f91143
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-10-08 12:19:14 +0530

    Create 12.cpp

---
+++ Patterns/12.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std ;
+int main(){
+	int a = 5 ;
+	int i ;
+	for(i=1; i<=a; i++){
+		for(int j = 1; j<=5-i; j++){
+			cout << " ";
+		}
+		for (int j=1; j<=i ; j++){
+			cout << "*";
+		}
+		cout << endl ;
+	}
+}
+
