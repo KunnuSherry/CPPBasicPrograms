eeb704f8db01784d0c2ff272015e2a78e96e5a8c
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-10-11 22:38:50 +0530

    Create 20.cpp

---
+++ Patterns/20.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std ;
+int main(){
+	int n = 5;
+	int i ;
+	for(i=1 ; i<=n ; i++){
+		int j ;
+		for( j=1; j<=i-1; j++){
+			cout << " " ;
+		}
+		for ( j=1; j<=5-i+1; j++){
+			cout << "* " ;
+		}
+		cout << "" << endl ;
+		
+	}
+	return 0;
+} 
+
+// * * * * *
+//  * * * *
+//   * * *
+//    * *
+//     *
