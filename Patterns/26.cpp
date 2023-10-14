b588419297feac16976c7bc0d38ce01a22094ee6
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2023-10-14 20:45:30 +0530

    Create 26.cpp

---
+++ Patterns/26.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std ;
+int main(){
+	int i;
+	for(i=1; i<=5; i++){
+		int j;
+		for(j=1; j<=4; j++){
+			if(i==1 || i==5 || j==1 || j==4){
+				cout << "*";
+			}
+			else{
+				cout << " ";
+			}
+		}
+		cout << "" << endl;
+	}	
+}
+
+// ****
+// *  *
+// *  *
+// *  *
+// ****
